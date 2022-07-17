aca9282d74950cbcd3810db3581220b530551e07
Akshay Sh <akshay.sharma@vinnovatetechnologies.com>
2022-07-17 16:58:03 +0530

    Create the Form with proper statemanagement

--- components/CreateForm/createForm.jsx
+++ components/CreateForm/createForm.jsx
@@ -1,61 +1,111 @@
+import { useState } from "react";
 
+const CreateForm = () => {
+  const [backColor, setBackColor] = useState("");
+  const [borderColor, setBorderColor] = useState("");
+  const [heading, setHeading] = useState("");
+  const [paragraph, setParagraph] = useState("");
+  const [brandlogo, setBrandLogo] = useState("");
+  const [brandImage, setBrandImage] = useState({});
 
-const createForm = () => {
-
-    const onSave = (e) => {
-        e.preventDefault()
-        console.log(e.target);
-    }
-
-    return (
-        <form className="w-1/2" onSubmit={onSave}>
-            <label className="block my-5 flex justify-between items-center">
-                <span className="block text-sm my-2 font-medium text-slate-700">Background Color</span>
-                <input type="text" value="" name="backColor" className="mt-1 block w-medium px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+  const onSave = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form className='w-1/2' onSubmit={onSave}>
+      <label className='block my-5 flex justify-between items-center'>
+        <span className='block text-sm my-2 font-medium text-slate-700'>
+          Background Color
+        </span>
+        <input
+          type='text'
+          value={backColor}
+          onChange={(e) => setBackColor(e.target.value)}
+          name='backColor'
+          className='mt-1 block w-medium px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                "/>
-            </label>
-            <label className="block my-5 flex justify-between items-center">
-                <span className="block text-sm my-2 font-medium text-slate-700">Border Color</span>
-                <input type="text" value="" name="borderColor" className="mt-1 block w-medium px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                '
+        />
+      </label>
+      <label className='block my-5 flex justify-between items-center'>
+        <span className='block text-sm my-2 font-medium text-slate-700'>
+          Border Color
+        </span>
+        <input
+          type='text'
+          value={borderColor}
+          onChange={(e) => setBorderColor(e.target.value)}
+          name='borderColor'
+          className='mt-1 block w-medium px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                "/>
-            </label>
-            <label className="block my-5 flex justify-between items-center">
-                <span className="block text-sm my-2 font-medium text-slate-700">Heading Text</span>
-                <input type="text" value="" name="heading" className="mt-1 block w-medium px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                '
+        />
+      </label>
+      <label className='block my-5 flex justify-between items-center'>
+        <span className='block text-sm my-2 font-medium text-slate-700'>
+          Heading Text
+        </span>
+        <input
+          type='text'
+          value={heading}
+          onChange={(e) => setHeading(e.target.value)}
+          name='heading'
+          className='mt-1 block w-medium px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                "/>
-            </label>
-            <label className="block my-5 flex justify-between items-center">
-                <span className="block text-sm my-2 font-medium text-slate-700">Paragraph Text</span>
-                <input type="text" value="" name="paragraph" className="mt-1 block w-medium px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                '
+        />
+      </label>
+      <label className='block my-5 flex justify-between items-center'>
+        <span className='block text-sm my-2 font-medium text-slate-700'>
+          Paragraph Text
+        </span>
+        <input
+          type='text'
+          value={paragraph}
+          onChange={(e) => setParagraph(e.target.value)}
+          name='paragraph'
+          className='mt-1 block w-medium px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                "/>
-            </label>
-            <label className="block my-5 flex justify-between items-center">
-                <span className="block text-sm my-2 font-medium text-slate-700">Brand Logo</span>
-                <input type="file" name="brandlogo" className="mt-1 block w-medium px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                '
+        />
+      </label>
+      <label className='block my-5 flex justify-between items-center'>
+        <span className='block text-sm my-2 font-medium text-slate-700'>
+          Brand Logo
+        </span>
+        <input
+          type='file'
+          name='brandlogo'
+          onChange={(e) => setBrandLogo(e.target.value)}
+          className='mt-1 block w-medium px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                "/>
-            </label>
-            <label className="block my-5 flex justify-between items-center">
-                <span className="block text-sm my-2 font-medium text-slate-700">Brand Image</span>
-                <input type="file" name="brandImage" className="block w-medium text-sm text-slate-500
+                '
+        />
+      </label>
+      <label className='block my-5 flex justify-between items-center'>
+        <span className='block text-sm my-2 font-medium text-slate-700'>
+          Brand Image
+        </span>
+        <input
+          type='file'
+          name='brandImage'
+          onChange={(e) => setBrandImage(e.target.value)}
+          className='block w-medium text-sm text-slate-500
                     file:mr-4 file:py-2 file:px-4
                     file:rounded-medium file:border-0
                     file:text-sm file:font-semibold
                     file:bg-violet-50 file:text-violet-700
                     hover:file:bg-violet-100
-                "/>
-            </label>
-            <div className="flex justify-center">
-                <button className="bg-violet-500 my-4 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-2xl w-1/4">
-                Save
-                </button>
-            </div>
-        </form>
-    );
+                '
+        />
+      </label>
+      <div className='flex justify-center'>
+        <button className='bg-violet-500 my-4 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-2xl w-1/4'>
+          Save
+        </button>
+      </div>
+    </form>
+  );
 };
 
-export default createForm;
+export default CreateForm;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
-import Head from 'next/head';
-import Product from '../components/Product';
-import Nav from '../components/Nav/Nav';
-import prisma from '../lib/prisma';
-import Link from 'next/link';
+import Head from "next/head";
+import Product from "../components/Product";
+import Nav from "../components/Nav/Nav";
+import prisma from "../lib/prisma";
+import Link from "next/link";
 
 export default function Home({ products }) {
   return (
     <div>
       <Head>
         <title>PlanetScale Next.js Quickstart</title>
-        <meta name="description" content="PlanetScale Quickstart for Next.js" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='PlanetScale Quickstart for Next.js' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className="p-10 mx-auto max-w-4xl">
-        <Link href="/">
-          <a className="text-6xl font-bold mb-4 text-center">Development branch</a>
+      <main className='p-10 mx-auto max-w-4xl'>
+        <Link href='/'>
+          <a className='text-6xl font-bold mb-4 text-center'>
+            Development branch
+          </a>
         </Link>
-        <p className="mb-20 text-xl text-center">
+        <p className='mb-20 text-xl text-center'>
           🔥 Shop from the hottest items in the world 🔥
         </p>
-        <div className="flex justify-center">
+        <div className='flex justify-center'>
           <Nav />
         </div>
-          {/* {products.map((product) => (
+        {/* {products.map((product) => (
             <Product product={product} key={product.id} />
           ))} */}
       </main>
@@ -33,19 +35,19 @@ export default function Home({ products }) {
   );
 }
 
-export async function getStaticProps(context) {
-  const data = await prisma.product.findMany({
-    include: {
-      category: true,
-    },
-  });
+// export async function getStaticProps(context) {
+//   // const data = await prisma.product.findMany({
+//   //   include: {
+//   //     category: true,
+//   //   },
+//   });
 
-  //convert decimal value to string to pass through as json
-  const products = data.map((product) => ({
-    ...product,
-    price: product.price.toString(),
-  }));
-  return {
-    props: { products },
-  };
-}
+//convert decimal value to string to pass through as json
+// const products = data.map((product) => ({
+//   ...product,
+//   price: product.price.toString(),
+// }));
+// return {
+//   props: { products },
+// };
+// }
