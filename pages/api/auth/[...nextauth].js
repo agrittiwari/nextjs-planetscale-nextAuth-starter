847e2cd76a8705c8942be8267d332b308e73504a
Agrit Tiwari <agrit1408@gmail.com>
2022-07-22 18:42:43 +0530

    pushing nextAuth Changes

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_cLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+})
