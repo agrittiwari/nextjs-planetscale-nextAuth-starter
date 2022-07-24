3ad60d667025f241dfc91f0887e795d3ae8d3c3d
Akshay Sh <akshay.sharma@vinnovatetechnologies.com>
2022-07-24 14:31:19 +0530

    api for incidents pages for add and get data

--- pages/add.js
+++ pages/add.js
@@ -1,28 +1,52 @@
-import Head from 'next/head'
-import React from 'react'
-import Form from '../components/InputForm'
-import Nav from '../components/Nav/Nav'
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
+import Form from "../components/InputForm";
+import Nav from "../components/Nav/Nav";
 
-const add = () => {
+const Add = () => {
+  const [author, setAuthor] = useState("");
+  const [event, setEvent] = useState("");
+  const [yearsAgo, setYearsAgo] = useState("");
+  const [country, setCountry] = useState("");
+  const [authorId, setauthorId] = useState("");
+
+  const handleSubmit = async () => {
+    const form = { event, yearsAgo, country, author, authorId };
+    try {
+      const response = await fetch("/api/addincident", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: form,
+      });
+      if (response.status === 200) {
+        console.log(await response.json());
+        setIncidents(await response.json().incidents);
+      } else {
+        console.log(
+          "There was ana error getting data",
+          await response.json().msg
+        );
+      }
+    } catch (error) {
+      console.log("There was an error getting data", error);
+    }
+  };
 
   return (
-    
     <div>
-        <Head>
+      <Head>
         <title>Share your Funny Incident/Memory with the world</title>
-        <meta name="description" content="Add your funny moment" />
-        <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main>
-            <Nav/>
+        <meta name='description' content='Add your funny moment' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main>
+        <Nav />
         <p>Add YOUR FUNNY iNCIDENT here</p>
-        <Form/>
-        </main>
-        <footer>
-
-        </footer>
+        <Form />
+      </main>
+      <footer></footer>
     </div>
-  )
-}
+  );
+};
 
-export default add
+export default Add;
