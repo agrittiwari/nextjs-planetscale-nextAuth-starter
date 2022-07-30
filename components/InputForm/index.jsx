1be9e20e891ebcce26614f79f9635a45c1b9599e
Akshay Sh <akshay.sharma@vinnovatetechnologies.com>
2022-07-30 22:02:59 +0530

    font weight increased of sign in event

--- components/InputForm/index.jsx
+++ components/InputForm/index.jsx
@@ -14,66 +14,62 @@ const Form = ({
   onSave,
 }) => {
   const { data: session, status } = useSession();
-  if (status === "unauthenticated")
-    return <span>Sign In to make your Entry</span>;
-  if (status === "loading") return <div className='loading' />;
-  if (status === "authenticated")
-    return (
-      <div className={Styles.formDiv}>
-        <form className='w-1/2' onSubmit={onSave}>
-          <label className={Styles.label}>Share your Funny Memory</label>
-          <textarea
-            type='text'
-            placeholder='Incidents ......'
-            value={event}
-            name='event'
-            onChange={(e) => setEvent(e.target.value)}
-            className={Styles.inputClass1}
-            width='48'
-            height='48'
-          ></textarea>
-          <div className={Styles.subForm}>
-            <div className={Styles.subFormPart}>
-              <label className={Styles.label}>
-                When did this funny story happened?
-              </label>
-              <input
-                className={Styles.inputClass2}
-                type='number'
-                min='1900'
-                max='2022'
-                step='1'
-                value={yearsAgo}
-                placeholder='like in 2021'
-                name='yearsAgo'
-                onChange={(e) => setYearsAgo(parseInt(e.target.value, 10))}
-              />
-            </div>
-            <div className={Styles.subFormPart}>
-              <label className={Styles.label}>Where are you from?</label>
-              <select
-                name='country'
-                className={Styles.inputClass2}
-                placeholder='Select any country'
-                onChange={(e) => {
-                  setCountry(e.target.value);
-                }}
-              >
-                {countries?.map((val) => (
-                  <option
-                    key={val.cca2}
-                    value={`${val.name.common},${val.flags.svg}`}
-                  >
-                    {val.name.common}
-                  </option>
-                ))}
-              </select>
-            </div>
+  return (
+    <div className={Styles.formDiv}>
+      <form className='w-1/2' onSubmit={onSave}>
+        <label className={Styles.label}>Share your Funny Memory</label>
+        <textarea
+          type='text'
+          placeholder='Incidents ......'
+          value={event}
+          name='event'
+          onChange={(e) => setEvent(e.target.value)}
+          className={Styles.inputClass1}
+          width='48'
+          height='48'
+        ></textarea>
+        <div className={Styles.subForm}>
+          <div className={Styles.subFormPart}>
+            <label className={Styles.label}>
+              When did this funny story happened?
+            </label>
+            <input
+              className={Styles.inputClass2}
+              type='number'
+              min='1900'
+              max='2022'
+              step='1'
+              value={yearsAgo}
+              placeholder='like in 2021'
+              name='yearsAgo'
+              onChange={(e) => setYearsAgo(parseInt(e.target.value, 10))}
+            />
           </div>
-          <input className={Styles.submit} type='submit' value='submit' />
-        </form>
-      </div>
-    );
+          <div className={Styles.subFormPart}>
+            <label className={Styles.label}>Where are you from?</label>
+            <select
+              name='country'
+              className={Styles.inputClass2}
+              placeholder='Select any country'
+              onChange={(e) => {
+                setCountry(e.target.value);
+              }}
+            >
+              {countries?.map((val) => (
+                <option
+                  key={val.cca2}
+                  value={`${val.name.common},${val.flags.svg}`}
+                >
+                  {val.name.common}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <input className={Styles.submit} type='submit' value='submit' />
+      </form>
+    </div>
+  );
 };
 
 export default Form;
