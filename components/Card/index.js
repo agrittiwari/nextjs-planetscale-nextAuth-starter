2e342270a77fd62f4760e429f24f5828227d1d31
Agrit Tiwari <agrit1408@gmail.com>
2022-07-30 16:29:54 +0530

    issues

--- components/Card/index.js
+++ components/Card/index.js
@@ -21,7 +21,8 @@ const Card = ({ val }) => {
         <p className={Style.name}>{user ? user.name : 'A Happy User'}</p>
         <p className={Style.country}>{country}</p>
       </div>
-      <div className={Style.div2part3}><p className={Style.time}>This Story is from {yearsAgo}</p></div>
+      <div className={Style.div2part3}>
+        <p className={Style.time}>YEAR:  {yearsAgo}</p></div>
       </div>  <div className={Style.div1}>
        
         <p className={Style.event}>{event}</p>
