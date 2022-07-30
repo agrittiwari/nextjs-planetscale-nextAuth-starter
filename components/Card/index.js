2030f30ef68948c2baf046369da27293f4bbac2d
Agrit Tiwari <agrit1408@gmail.com>
2022-07-30 21:09:34 +0530

    done

--- components/Card/index.js
+++ components/Card/index.js
@@ -8,30 +8,6 @@ const Card = ({ val }) => {
   return (
     <div className={Style.wrap}>
       <div className={Style.mainDiv}>
-    
-      <div className={Style.div2}>
-      <div className={Style.div2part1}>
-      <img
-          className={Style.userImg}
-          src={
-            user
-              ? user.image
-              : "https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg"
-          }
-          alt='user Image'
-        />
-      </div>
-      <div className={Style.div2part2}>
-        <p className={Style.name}>{user ? user.name : 'A Happy User'}</p>
-        <p className={Style.country}>{country}</p>
-      </div>
-      <div className={Style.div2part3}>
-        <p className={Style.time}>YEAR:  {yearsAgo}</p></div>
-
-      </div><div className={Style.dashDiv}/>  <div className={Style.div1}>
-       
-        <p className={Style.event}>{event}</p>
-             
         <div className={Style.div2}>
           <div className={Style.div2part1}>
             <img
@@ -54,12 +30,12 @@ const Card = ({ val }) => {
           <div className={Style.div2part3}>
             <p className={Style.time}>YEAR: {yearsAgo}</p>
           </div>
-        </div>{" "}
+        </div>
+        <div className={Style.dashDiv}/>
         <div className={Style.div1}>
           <p className={Style.event}>{event}</p>
         </div>
       </div>
-    </div>
     </div>
   );
 };
